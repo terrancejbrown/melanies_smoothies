20b0753fbf8373c5e4647071766f931cde6b8484
terrancejbrown <61844985+terrancejbrown@users.noreply.github.com>
2025-06-18 10:16:17 -0600

    removing SF from bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,5 +56,4 @@ if ingredients_list:
 
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
