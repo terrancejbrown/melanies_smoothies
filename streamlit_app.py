19579144a268d7ebb3ff5f24175b95708321a216
terrancejbrown <61844985+terrancejbrown@users.noreply.github.com>
2025-06-18 10:08:13 -0600

    moving smoothieFroot down

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,7 @@ st.write(
   """
 )
 
-#SmoothieFroot
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+
 
 
 name_on_order = st.text_input("Name on Smoothie")
@@ -48,6 +46,12 @@ if ingredients_list:
     
     time_to_insert = st.button('Submit Order')
 
+  
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!'+ ', ' +name_on_order+ '!', icon="✅")
+
+
+#SmoothieFroot
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
