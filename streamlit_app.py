3133283ea5cfcc878941c38055b09149ca1f64fa
terrancejbrown <61844985+terrancejbrown@users.noreply.github.com>
2025-06-18 10:13:06 -0600

    added SF dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,4 +54,5 @@ if ingredients_list:
 
 #SmoothieFroot
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
